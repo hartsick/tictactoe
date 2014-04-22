91e55875a5075d53c8d80a93c3baddb0162732b7
Christa Hartsock <christa.hartsock@gmail.com>
2014-04-22 09:14:21 -0700

    adding tic tac toe files

---
+++ js/3dapp.js
@@ -0,0 +1,114 @@
+function TTTController($scope){
+
+	// instantiate players
+	$scope.players = [
+		{
+			name: "Player 1",
+			character: "X",
+			value: 1,
+			wins: 0,
+			color: 'rgba(0,0,200, .5)'
+		},
+		{
+			name: "Player 2",
+			character: "O",
+			value: -1,
+			wins: 0,
+			color: 'rgba(200,0,0,.5)'
+		}
+	];
+	
+	// set default number of players
+	numPlayers = 2;
+
+	var playerIndex = 0;
+	var currentPlayer = $scope.players[playerIndex];
+
+	// // instantiate cube faces
+	// $scope.cube = [
+	// 	{
+	// 		face: "front",
+	// 		board: new Array(9)
+	// 	},
+	// 	{
+	// 		face: "back",
+	// 		board: new Array(9)
+	// 	},
+	// 	{
+	// 		face: "top",
+	// 		board: new Array(9)
+	// 	},
+	// 	{
+	// 		face: "bottom",
+	// 		board: new Array(9)
+	// 	},
+	// 	{
+	// 		face: "left",
+	// 		board: new Array(9)
+	// 	},
+	// 	{
+	// 		face: "right",
+	// 		board: new Array(9)
+	// 	}
+	// ];
+
+	$scope.cubeFaces = ["front", "back", "top", "bottom", "left", "right"];
+	$scope.cube = function(){
+		var cubeArray;
+		for (var i = 0; i < 6; i++){
+			cubeArray[i] = {
+				face: cubeFaces[i],
+				board: new Array(9)
+			}
+		}
+		console.log(cubeArray);
+		return cubeArray;
+	};
+
+	/* 
+	 * Place marker in specified div
+	 */
+	$scope.placeMarker = function(clickedFace, $index, $event){
+		$event.target.style.backgroundColor = currentPlayer.color;
+		if ($scope.cube.face == clickedFace ){
+
+		}
+	}
+
+	/*
+	 * Switch current player
+	 */
+	$scope.switchPlayer = function(){
+		// increment player index
+		playerIndex++;
+
+		// if index is greater than the number of players, bring it back around
+		if (playerIndex > numPlayers - 1){
+			playerIndex = 0;
+		}
+
+		// update current player
+		currentPlayer = $scope.players[playerIndex];
+		console.log("Current Player: " + currentPlayer.name);
+	};
+
+
+	/*
+	 * Click handler for cube
+	 */
+	$scope.clicker = function(clickedFace, $index, $event) {
+		console.log("Cell Index:" + clickedFace + $index);
+		console.log("Player color: " + currentPlayer.color);
+		$scope.placeMarker(clickedFace, $index, $event);
+		$scope.switchPlayer();
+	};
+
+	/*
+	 * Rotate the cube
+	 */
+	$scope.controls = ["up", "down", "right", "left"];
+	$scope.rotateCube = function($id, $event){
+		console.log("ID: " + $id);
+	};
+
+};
---
+++ js/script.js
@@ -0,0 +1,183 @@
+// Initialize board & move counters
+var boardHeight;
+var boardTotal;
+var boardArray;
+var totalMoves;
+
+// Initialize players
+var player1 = {points: 5, name: "Player One", wins: 0};
+var player2 = {points: 7, name: "Player Two", wins: 0};
+
+var gameEnd;
+var player; 
+
+// Begin Tic Tac Toe!
+window.onload = function() {
+	buildBoard();
+}
+
+function buildBoard() {
+	// Initialize variables
+	totalMoves = 0
+
+	// Initialize board & move counter
+	boardHeight = 3;
+	boardTotal = boardHeight * boardHeight;
+	boardArray = new Array(boardTotal);
+
+	gameEnd = false;
+	player = player1;
+
+	// Get all children to row divs
+	boardHTML = document.querySelectorAll("div div div");
+
+	// Cycle through and add click handler
+	for ( var row = 0; row < boardHTML.length; row ++){
+		boardHTML[row].onclick=takeTurn;
+	}
+}
+
+function takeTurn() {
+	// if game hasn't ended yet...
+	if (gameEnd == false){
+		if (placeMarker(this)){
+			checkForWin();
+			switchPlayer();
+		}
+	}
+	else {
+		return;
+	}
+	console.log(gameEnd);
+}
+
+function placeMarker(clickedDiv){
+	// If space unoccupied, place appropriate marker & switch players
+	if (clickedDiv.innerHTML==""){
+
+		// store player# in div's array position
+		boardArray[clickedDiv.id] = player.points;
+
+		// place marker & switch players
+		if (player == player1){
+			clickedDiv.innerHTML="X";
+		}
+		else {
+			clickedDiv.innerHTML="O";
+		}
+		// increment total # of moves
+		totalMoves++;
+		return true;
+	}
+	// Else, tell them to play by the rules
+	else {
+		alert("Try an empty square!");
+		return false;
+	}
+}
+
+function checkForWin() {
+	// don't bother checking for win before it's possible
+	if (totalMoves > (boardHeight * 2) - 2){
+
+		// declare winCondition
+		var winCondition;
+
+		// CHECK ROWS
+		// iterate through rows
+		for (var row = 0; row < boardHeight; row++){
+			winCondition = 0;
+
+			// check each div of row
+			for (var index = row * boardHeight; index < (row + 1) * boardHeight; index++){
+				gameEnd = hasPlayerWon(index);
+			}
+		}
+
+		// CHECK COLUMNS
+		// iterate through columns
+		for (var row = 0; row < boardHeight; row++){
+			// reset win condition
+			winCondition = 0;
+			// iterate through each div in column
+			for (var index = row; index < boardTotal; index = index + boardHeight){
+				gameEnd = hasPlayerWon(index);
+			}
+		}
+
+		// CHECK DIAGONALS
+		
+		// iterate through left-facing diagonal
+		for (var row = 0; row < boardHeight; row++){
+			// reset win condition
+			winCondition = 0;
+			for (var index = 0; index < boardTotal; index = index + (boardHeight + 1) ){
+				gameEnd = hasPlayerWon(index);
+				if (gameEnd){
+					break;
+				}
+			}
+		}
+		
+		// iterate through right-facing diagonal
+		for (var row = 0; row < boardHeight; row++){
+			// reset win condition
+			winCondition = 0;
+
+			for (var index = boardHeight - 1; index < boardTotal; index = index + (boardHeight - 1)){
+				gameEnd = hasPlayerWon(index);
+				if (gameEnd){
+					break;
+				}
+			}
+		}
+	}	
+
+	function hasPlayerWon(i){
+		// if null value found in array, break 
+		if (!boardArray[i]){
+			return false;
+		}
+		// otherwise, start the tally & check for win
+		else {
+
+			// add current cell value to winCondition
+			winCondition += boardArray[i];
+
+			// check winCondition for value of 15 for player1 win, or 21 for player2 win
+			if (winCondition == player1.points * boardHeight || winCondition == player2.points * boardHeight){
+				document.getElementById("message").innerHTML = player.name + " won!";
+
+				// increment win counter for appropriate player
+				if (winCondition == player1.points * boardHeight ){
+					player1.wins++;
+				}
+				else {
+					player2.wins++;
+				}
+				console.log("Hi!");
+
+				return true;
+			}
+
+			// if final move and no win, declare tie
+			else if (totalMoves == boardTotal){
+				document.getElementById("message").innerHTML = "You tied!";
+				return true;
+			}
+
+			else {
+				return false;
+			}
+		}			
+	}
+}
+
+function switchPlayer(){
+	if (player == player1){
+		player = player2;
+	}
+	else {
+		player = player1;
+	}
+}
